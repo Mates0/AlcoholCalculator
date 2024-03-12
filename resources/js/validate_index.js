f20e32e2dbed18545b25208dcc520de571340e9a
Mates0 <matyas.vanko@educanet.cz>
2024-03-12 13:48:35 +0100

    index validation part 1

--- resources/js/validate_index.js
+++ resources/js/validate_index.js
@@ -1,16 +1,49 @@
 export function validateIndex(weight,timeInput,addedAlcohol) {
+    let weightForm = document.getElementById("weightForm")
+    let genderForm = document.getElementById("genderForm")
+    let timeForm = document.getElementById("timeForm")
+    let noWeight = document.getElementById("no-weight-placeholder")
+    let noGender = document.getElementById("no-gender-placeholder")
+    let noTime = document.getElementById("no-time-placeholder")
+
     if (!weight) {
-        alert("Prosím zadejte váhu");
+        weightForm.style.border = "3px solid red";
+        noWeight.className = "d-block";
+        noWeight.innerHTML = "Prosím zadejte váhu";
         return false;
+    } else {
+        noWeight.className = "d-none";
+        weightForm.style.border = "none";
     }
+
     if (weight < 0) {
-        alert("Váha nemůže být záporná");
+        weightForm.style.border = "3px solid red";
+        noWeight.className = "d-block";
+        noWeight.innerHTML = "Váha nemůže být záporná";
         return false;
+    } else {
+        noWeight.className = "d-none"
+        weightForm.style.border = "none";
     }
+
+    if (genderForm.value !== "Muž" && genderForm.value !== "Žena") {
+        genderForm.style.border = "3px solid red";
+        noGender.className = "d-block";
+        console.log("test");
+    } else {
+        genderForm.style.border = "none";
+        noGender.className = "d-none";
+    }
+
     if (!timeInput) {
-        alert("Prosím zadejte čas");
+        timeForm.style.border = "3px solid red";
+        noTime.className = "d-block";
         return false;
+    } else {
+        timeForm.style.border = "none";
+        noTime.className = "d-none";
     }
+
     if (!addedAlcohol || addedAlcohol.length === 0) {
         alert("Prosím přidejte alkohol");
         return false;
